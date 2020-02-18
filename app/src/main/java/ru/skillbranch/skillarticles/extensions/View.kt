aa1483eab08af8a2e71762ea0b00f3ea9c228db2
Shaukat Salakhutdinov <sshaukat@mail.ru>
2020-02-18 23:54:12 +0300

    hometask_4 ver 3.3.2

--- app/src/main/java/ru/skillbranch/skillarticles/extensions/View.kt
+++ app/src/main/java/ru/skillbranch/skillarticles/extensions/View.kt
@@ -2,8 +2,12 @@ package ru.skillbranch.skillarticles.extensions
 
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.marginBottom
+import androidx.core.view.marginLeft
+import androidx.core.view.marginRight
+import androidx.core.view.marginTop
 
-fun View.setMarginOptionally(top: Int = 0, right: Int = 0, bottom: Int = 0, left: Int = 0) {
+fun View.setMarginOptionally(left:Int = marginLeft, top : Int = marginTop, right : Int = marginRight, bottom : Int = marginBottom) {
     (layoutParams as ViewGroup.MarginLayoutParams).run {
         leftMargin = left
         rightMargin = right
