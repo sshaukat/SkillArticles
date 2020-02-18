fb600c7f3edb54b1b5ece6bdb137f01a299cde6f
Shaukat Salakhutdinov <sshaukat@mail.ru>
2020-02-18 23:12:59 +0300

    hometask_4 ver 3.2

--- app/src/main/java/ru/skillbranch/skillarticles/data/local/PrefManager.kt
+++ app/src/main/java/ru/skillbranch/skillarticles/data/local/PrefManager.kt
@@ -3,7 +3,7 @@ package ru.skillbranch.skillarticles.data.local
 
 import android.content.Context
 import android.content.SharedPreferences
-import android.preference.PreferenceManager
+import androidx.preference.PreferenceManager
 
 
 class PrefManager(context: Context) {
